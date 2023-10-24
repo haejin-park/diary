50a4927623d525271df347b390d11c0c094faba0
haejin <luckyhaejin1126@gmail.com>
2023-10-24 12:53:04 +0900

    using array - list rendering

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
 
-
+const dummyList = () => [
+  {
+    id: 1,
+    author: "박해진",
+    content: "일기1",
+    emotion: 1,
+    created_date: new Date().getTime()
+  },
+  {
+    id: 2,
+    author: "박해진",
+    content: "일기2",
+    emotion: 2,
+    created_date: new Date().getTime()
+  },
+  {
+    id: 3,
+    author: "박해진",
+    content: "일기3",
+    emotion: 3,
+    created_date: new Date().getTime()
+  }
+]
 const App = () => {
   return (
       <div className="App">
         <DiaryEditor />
+        <DiaryList diaryList={dummyList()} />
       </div>
   );
 }
