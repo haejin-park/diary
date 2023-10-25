44898fb38c265a6d5f82c76ba6f9bf539e2bcb1f
haejin <luckyhaejin1126@gmail.com>
2023-10-25 13:43:58 +0900

    using array - update date

--- src/App.js
+++ src/App.js
@@ -18,16 +18,23 @@ const App = () => {
     dataId.current += 1;
     setData([newItem, ...data]);
   };
-  const onDelete = (targetId) => {
+  const onRemove = (targetId) => {
     console.log(`${targetId}번쨰 일기가 삭제되었습니다.`);
     const newDiaryList = data.filter((it) => it.id !== targetId);
     setData(newDiaryList);
     console.log(newDiaryList);
   }
+  const onEdit = (targetId, newContent) => {
+    setData(
+      data.map((it) =>
+        it.id === targetId ? {...it, content: newContent}: it
+      )
+    )
+  }
   return (
       <div className="App">
         <DiaryEditor onCreate={onCreate}/>
-        <DiaryList onDelete={onDelete} diaryList={data} />
+        <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
       </div>
   );
 }
